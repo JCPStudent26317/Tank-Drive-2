5200e124d4753923566dc7c4b97146cbe74a556c
Jesuit Student 26317 <26317@jcpstudents.org>
2024-10-24 16:37:27 -0500

    Further debug

--- src/main/java/frc/robot/subsystems/DrivebaseSubsys.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsys.java
@@ -34,6 +34,7 @@ public class DrivebaseSubsys extends SubsystemBase {
   public void setSpeeds(double speedL, double speedR){
     motorL1.set(speedL);
     motorR1.set(speedR);
+    System.out.println("DRIVETRAIN: " + speedL + ", " + speedR);
   }
 
   /*
